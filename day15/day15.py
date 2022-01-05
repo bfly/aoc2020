324a2ab0fc0e0b8af44beffece96108db1c0bb71
bfly <billfly@programmer.net>
2022-01-04 19:00:01 -0600

    Commit day15.

--- day15/day15.py
+++ day15/day15.py
@@ -1,82 +1,45 @@
+import collections
 import re
-
-FIELD_RE = re.compile(r'^([^:]+): (\d+)-(\d+) or (\d+)-(\d+)$')
+from typing import NamedTuple, Tuple, Generator, Dict, List
 
 
 def part1(_lines):
-    x = _lines.split('\n\n')
-    print(x)
-    # fields, my_ticket, nearby = _lines.split('\n\n')
+    prev_seen: Dict[int, List[int]] = collections.defaultdict(list)
+    numbers = [int(n) for n in _lines.strip().split(',')]
+    n = 0
 
-    field_ranges = {}
-    for line in fields.splitlines():
-        m = FIELD_RE.match(line)
-        assert m
-        field_ranges[m[1]] = (int(m[2]), int(m[3]), int(m[4]), int(m[5]))
+    for turn in range(2020):
+        if turn < len(numbers):
+            n = numbers[turn]
+        elif len(prev_seen[n]) == 1:
+            n = 0
+        else:
+            n = prev_seen[n][-1] - prev_seen[n][-2]
 
-    invalid = 0
-    for line in nearby.splitlines()[1:]:
-        for n_s in line.split(','):
-            n = int(n_s)
-            for b1, e1, b2, e2 in field_ranges.values():
-                if b1 <= n <= e1 or b2 <= n <= e2:
-                    break
-            else:
-                invalid += n
+        prev_seen[n].append(turn)
 
-    return invalid
+    return n
 
 
 def part2(_lines):
-    fields, my_ticket, nearby = _lines.split('\n\n')
+    seen2: Dict[int, int] = {}
+    seen1: Dict[int, int] = {}
+    numbers = [int(n) for n in _lines.strip().split(',')]
+    n = 0
 
-    field_ranges = {}
-    for line in fields.splitlines():
-        m = FIELD_RE.match(line)
-        assert m
-        field_ranges[m[1]] = (int(m[2]), int(m[3]), int(m[4]), int(m[5]))
-
-    my_ticket_n = [int(n_s) for n_s in my_ticket.splitlines()[1].split(',')]
-
-    valid_tickets = []
-    for line in nearby.splitlines()[1:]:
-        ticket = [int(n_s) for n_s in line.split(',')]
-        for n in ticket:
-            for b1, e1, b2, e2 in field_ranges.values():
-                if b1 <= n <= e1 or b2 <= n <= e2:
-                    break
-            else:
-                break
+    for turn in range(30_000_000):
+        if turn < len(numbers):
+            n = numbers[turn]
+        elif n not in seen2:
+            n = 0
         else:
-            valid_tickets.append(ticket)
+            n = seen1[n] - seen2[n]
 
-    possible_at_position = {
-        pos: {
-            k for k, (b1, e1, b2, e2) in field_ranges.items()
-            if all(
-                b1 <= ticket[pos] <= e1 or b2 <= ticket[pos] <= e2
-                for ticket in valid_tickets
-            )
-        }
-        for pos in range(len(valid_tickets[0]))
-    }
+        if n in seen1:
+            seen2[n] = seen1[n]
+        seen1[n] = turn
 
-    positions = {}
-    while possible_at_position:
-        for k, v in tuple(possible_at_position.items()):
-            if len(v) == 1:
-                key, = v
-                positions[key] = k
-                possible_at_position.pop(k)
-                for _v in possible_at_position.values():
-                    _v.discard(key)
-
-    ret = 1
-    for key, pos in positions.items():
-        if key.startswith('departure '):
-            ret *= my_ticket_n[pos]
-
-    return ret
+    return n
 
 
 def main(_fn):
